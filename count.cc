8cafe84b794609569eda7efe6742b4b6da4dc68c
kyeongchan317 <jac07171@gmail.com>
2023-09-05 20:12:15 +0900

    add count.cc

--- count.cc
+++ count.cc
@@ -3,4 +3,8 @@
 void count_number() {
     // lab2 //
     // do it yourself //
+    for(int i = 0; i <= 10; i++)
+	    std::cout << i << std::endl;
+    std::cout << 2019311122 << std::endl;
+    return;
 }
